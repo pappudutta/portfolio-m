b7dfc309eaa13e03514404f6413732d5bcacb608
Pappu Dutta <pappudutta279@gmail.com>
2023-09-16 01:05:44 +0530

    style: Header Menu update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="flex justify-between my-auto py-10 mx-12 opacity-70 text-sm font-light">
+      <div className="flex gap-2 flex-col md:flex-row justify-between my-auto py-10 mx-12 opacity-70 text-sm font-light">
         <p className="">
           © 2023 Developed by •{" "}
           <a
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ import More from "../assets/asset 28.svg";
 const Header = () => {
   return (
     <>
-      <div className="flex  fixed z-20 bottom-10 w-[100%] items-center mx-auto px-2">
-        <div className="inline-flex gap-1 md:gap-2 box border border-[#292929] m-5 p-2 rounded-full bg-[#292929c4] mx-auto">
+      <div className="menu-center flex fixed bottom-10 w-[50%] left-[50%] translate-x-[-50%, 50%]">
+        <div className="inline-flex gap-1 md:gap-2 box border border-[#292929] p-2 rounded-full backdrop-blur-[2px] bg-[#292929dd] mx-auto ">
           <div className="">
             <img
               className="w-12 rounded-full border border-[#383838] hover:border-[#707070] transition ease-in-out delay-100 p-1"
@@ -20,35 +20,35 @@ const Header = () => {
           </div>
           <div className="w-12 flex flex-col items-center bg-[#151515] hover:bg-[#3e3e3e] rounded-full border border-[#3e3e3e] hover:border-[#3e3e3e] transition ease-in-out delay-100 p-1">
             <img
-              className="w-[21px] mt-2 text-white opacity-100"
+              className="w-[21px] mt-1 md:mt-2 text-white opacity-100"
               src={Projects}
               alt=""
             />
           </div>
           <div className="w-12 flex flex-col items-center bg-[#151515] hover:bg-[#3e3e3e] rounded-full border border-[#3e3e3e] hover:border-[#3e3e3e] transition ease-in-out delay-100 p-1">
             <img
-              className="w-[21px] mt-2 text-white opacity-100"
+              className="w-[21px] mt-1 md:mt-2 text-white opacity-100"
               src={Stack}
               alt=""
             />
           </div>
           <div className="w-12 flex flex-col items-center bg-[#151515] hover:bg-[#3e3e3e] rounded-full border border-[#3e3e3e] hover:border-[#3e3e3e] transition ease-in-out delay-100 p-1">
             <img
-              className="w-[21px] mt-2 text-white opacity-100"
+              className="w-[21px] mt-1 md:mt-2 text-white opacity-100"
               src={Notes}
               alt=""
             />
           </div>
           <div className="w-12 flex flex-col items-center bg-[#151515] hover:bg-[#3e3e3e] rounded-full border border-[#3e3e3e] hover:border-[#3e3e3e] transition ease-in-out delay-100 p-1">
             <img
-              className="w-[21px] mt-2 text-white opacity-100"
+              className="w-[21px] mt-1 md:mt-2 text-white opacity-100"
               src={Misc}
               alt=""
             />
           </div>
           <div className="w-12 flex flex-col items-center bg-[#151515] hover:bg-[#3e3e3e] rounded-full border border-[#3e3e3e] hover:border-[#3e3e3e] transition ease-in-out delay-100 p-1">
             <img
-              className="w-[21px] mt-2 text-white opacity-100 "
+              className="w-[21px] mt-1 md:mt-2 text-white opacity-100"
               src={More}
               alt=""
             />
